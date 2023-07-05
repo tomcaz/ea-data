93e2d59fa2343aee3d4acb3ca423943d456d9ac8
Zaw Htun <tom.tomcaz@gmail.com>
2023-07-04 23:50:08 -0700

    Added Test Module

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const cors = require('cor');
+
+const app = express()
+const port = 3000
+app.use(cors());
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
