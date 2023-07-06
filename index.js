1d367bab1703f8a138b10842f681a5343831aac2
Zaw Htun <tom.tomcaz@gmail.com>
2023-07-06 11:26:21 -0700

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express()
 const port = 4000
 app.use(cors());
 app.use(bodyParser.json());
-app.use('/api/tasks',require('./src/end-points/task.controller.js'))
+app.use('/',require('./src/end-points/task.controller.js'))
 
 // app.listen(port, () => {
 //   console.log(`Example app listening on port ${port}`)
