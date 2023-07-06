834f2feb6895b904feabc9ae58ac55f08502058b
Zaw Htun <tom.tomcaz@gmail.com>
2023-07-05 22:05:21 -0700

    Change backend Port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors');
 var bodyParser = require('body-parser')
 const app = express()
-const port = 3000
+const port = 4000
 app.use(cors());
 app.use(bodyParser.json());
 app.use('/api/tasks/',require('./src/end-points/task.controller.js'))
