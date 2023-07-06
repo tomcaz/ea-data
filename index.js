3c0f576b51544ce02a7d4a6a3f52cc88597a6d5b
Zaw Htun <tom.tomcaz@gmail.com>
2023-07-06 11:21:10 -0700

    adding functions

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use('/api/tasks/',require('./src/end-points/task.controller.js'))
 
+app.use('/.netlify/functions/server', require('./src/end-points/task.controller.js'))
+app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+
 // app.listen(port, () => {
 //   console.log(`Example app listening on port ${port}`)
 // })
