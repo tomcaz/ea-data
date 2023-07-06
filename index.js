7133f8f672640d3e128e536d859ff2719f3a34c2
Zaw Htun <tom.tomcaz@gmail.com>
2023-07-06 11:24:17 -0700

    test

--- index.js
+++ index.js
@@ -6,15 +6,12 @@ const app = express()
 const port = 4000
 app.use(cors());
 app.use(bodyParser.json());
-app.use('/api/tasks/',require('./src/end-points/task.controller.js'))
-
-app.use('/.netlify/functions/server', require('./src/end-points/task.controller.js'))
-app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+app.use('/api/tasks',require('./src/end-points/task.controller.js'))
 
 // app.listen(port, () => {
 //   console.log(`Example app listening on port ${port}`)
 // })
 
 
-module.exports.handler = serverless(app);
+module.exports.handler = serverless(api);
 module.exports = app; // for testing
